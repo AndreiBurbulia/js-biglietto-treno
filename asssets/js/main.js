0e11c502dd081010532adf13f1df18de5dab8a4e
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-23 15:27:09 +0200

    File e struttura

---
+++ asssets/js/main.js
@@ -0,0 +1,14 @@
+//Chiedere l'eta del passegero
+
+//chiedere quanti km vuole percorere in totale
+
+//calcolare il prezzo del biglietto
+
+//prezzo biglietto 0.21 per km
+
+//se ha meno di 18 anni sconto del 20%
+
+//se ha piu di 65 anni sconto del 40%
+
+//l'output del prezzo deve avere solo due cifre decimali
+//con "(qui ci va il numero da arrotondare) .toFixed(qui vanno specificate quanti numeri dopo la virgola)"
